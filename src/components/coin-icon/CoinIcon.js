2c5c562bb703b8c26c05094dad7f0834b53c4651
Alasdair <ay@coinburp.com>
2021-03-03 10:21:51 +0000

    Coin row styling

--- src/components/coin-icon/CoinIcon.js
+++ src/components/coin-icon/CoinIcon.js
@@ -8,7 +8,7 @@ import CoinIconIndicator from './CoinIconIndicator';
 import { useColorForAsset } from '@rainbow-me/hooks';
 import { getTokenMetadata, isETH, magicMemo } from '@rainbow-me/utils';
 
-export const CoinIconSize = 40;
+export const CoinIconSize = 48;
 
 const StyledCoinIcon = styled(ReactCoinIcon)`
   opacity: ${({ isHidden }) => (isHidden ? 0.4 : 1)};
